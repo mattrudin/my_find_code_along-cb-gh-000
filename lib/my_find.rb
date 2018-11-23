9d136eab8606bd189275b5775ca014291921e5e9
Matthias Rudin <matthias.rudin@live.com>
2018-11-23 04:11:44 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,7 +3,7 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length
-    return yield(collection[i]) ? collection[i] : next
+    yield(collection[i]) ? collection[i] : next
     i += 1
   end
 end
